26d018dd9d9f71fe45593a26b47c7b2dd02329a2
tony linux <13antonysun@gmail.com>
2025-09-25 12:17:48 +0530

    refactor: redirecting routes to router

--- src/app.js
+++ src/app.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const helmet = require('helmet'); 
 const cors = require('cors');
-const config = require('./config'); // configuration service
+const config = require('./config'); // @description Configuration service for environment variables
+const routes = require('./routes'); // @description Main API router
 
-// express
 const app = express();
 
 // MIDDLEWARES
-app.use(helmet()); // using 'helmet' middleware for HTTPS
-app.use(cors()); // cors
-app.use(express.json()); // json handling requests
-app.use(express.urlencoded({ extended: true })); //urlencoded data parsing
+app.use(helmet()); // @description using 'helmet' middleware for security headers
+app.use(cors()); // @description Enables Cross-Origin Resource Sharing
+app.use(express.json()); // @description Parses incoming requests with JSON payloads
+app.use(express.urlencoded({ extended: true })); // @description Parses incoming requests with URL-encoded payloads
+
+// Mount API routes
+app.use('/api', routes);
 
 // PORT from config service
 const PORT = config.port;
 
-// testing purposes
-app.get('/', (req, res) => {
-  res.send('Lead Qualification Backend is running!');
-});
 
 // Server Listening to PORT
 app.listen(config.port, () => {
