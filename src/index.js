e2694474a8660fa2058ad5215790096c6854c097
tony linux <13antonysun@gmail.com>
2025-09-25 11:03:14 +0530

    feat: create basic Express server

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
+// imports
 const express = require('express');
-const helmet = require('helmet');
+const helmet = require('helmet'); 
 const cors = require('cors');
 require('dotenv').config();
 
+// express
 const app = express();
 
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// MIDDLEWARES
+app.use(helmet()); // using 'helmet' middleware for HTTPS
+app.use(cors()); // cors
+app.use(express.json()); // json handling requests
+app.use(express.urlencoded({ extended: true })); //urlencoded data parsing
 
+// PORT 
 const PORT = process.env.PORT || 3000;
 
+// testing purposes
 app.get('/', (req, res) => {
   res.send('Lead Qualification Backend is running!');
 });
 
+// Server Listening to PORT
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+// export this app
 module.exports = app;
