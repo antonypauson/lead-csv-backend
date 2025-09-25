b3418ce66572e2abebb0987c9e34e91878478f0a
tony linux <13antonysun@gmail.com>
2025-09-25 12:53:56 +0530

    feat: in memory offer service

---
+++ src/services/offer.service.js
@@ -0,0 +1,42 @@
+const { v4: uuidv4 } = require('uuid'); // generating unique IDs
+
+/**
+ * @typedef {object} Offer
+ * @property {string} id - Unique identifier for the offer.
+ * @property {string} name - The name of the offer.
+ * @property {string[]} value_props - Array of value propositions.
+ * @property {string[]} ideal_use_cases - Array of ideal use cases.
+ */
+
+/**
+ * in-memory storage for offers
+ * @type {Offer[]}
+ */
+const offers = [];
+
+/**
+ * Creates a new offer and stores it in memory.
+ * @param {object} offerData - The data for the new offer (name, value_props, ideal_use_cases).
+ * @returns {Offer} The created offer with a unique ID.
+ */
+function createOffer(offerData) {
+  const newOffer = {
+    id: uuidv4(),
+    ...offerData,
+  };
+  offers.push(newOffer);
+  return newOffer;
+}
+
+/**
+ * Retrieves all stored offers.
+ * @returns {Offer[]} An array of all offers.
+ */
+function getAllOffers() {
+  return offers;
+}
+
+module.exports = {
+  createOffer,
+  getAllOffers,
+};
